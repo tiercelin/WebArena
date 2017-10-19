1047c5679ae7552f12134d58d8ab49ca3c75d2c4
tomahawk44 <thomasolive4@gmail.com>
2017-10-19 18:06:28 +0200

    Players Entity class Players Table class updates

---
+++ webarena/src/Model/Entity/Players.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+class Players extends Entity {
+    
+    /**
+     * Prevent id to be mass-assigned or assigned by the user (auto incrementation has to be respected)
+     */
+    protected $_accessible = [
+        '*' => true,
+        'id' => false
+    ];
+     
+    /**
+     * Do not show hash passowrds to any one !!
+     */
+   protected $_hidden = ['password'];
+   
+}
