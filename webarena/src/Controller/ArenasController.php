f43557d1211a69f9fa1d77a618ffa5fcad97c768
pl <pierrelouis.imbert@gmail.com>
2017-10-21 19:28:36 +0200

    petite update fighter

--- webarena/src/Controller/ArenasController.php
+++ webarena/src/Controller/ArenasController.php
@@ -31,7 +31,22 @@ public function diary()
 
 public function fighter()
 {
+    $this->set('myname', "Julien Falconnet");
 
+    $this->loadModel('Fighters');
+
+    $entity = $this->Fighters->getFighter('545f827c-576c-4dc5-ab6d-27c33186dc3e');
+    $this->set('MES', $entity);
+    
+    $this->set('id_f', $entity->id);
+    $this->set('name_f', $entity->name);
+    $this->set('lvl_f', $entity->level);
+    $this->set('exp_f', $entity->exp);
+    
+    $this->set('sight_f', $entity->skill_sight);
+    $this->set('str_f', $entity->skill_strength);
+    $this->set('health_f', $entity->skill_health);
+    
 }
 
 public function login()
--- webarena/src/Template/Arenas/fighter.ctp
+++ webarena/src/Template/Arenas/fighter.ctp
@@ -1,1 +1,15 @@
 <?php $this->assign('title', 'Fighter');?>
+
+  Bienvenue <?php echo $myname;?> dans WebArena <br>
+  
+  //Test: <?php echo $MES;?> <br>
+  
+    Test: <?php echo $id_f;?> <br>
+      Test: <?php echo $name_f;?> <br>
+        Test: <?php echo $lvl_f;?> <br>
+          Test: <?php echo $exp_f;?> <br>
+            Test: <?php echo $sight_f;?> <br>
+              Test: <?php echo $str_f;?> <br>
+                Test: <?php echo $health_f;?> <br>
+
+  The strongest character is: <br>
