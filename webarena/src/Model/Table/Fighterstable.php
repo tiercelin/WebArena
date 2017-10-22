33653fc11103ae5a7e0094cccfaed45f2d0ee032
Mariane Champalier <marianec@hotmail.fr>
2017-10-22 10:56:27 +0200

    commit débile pour corriger les problèmes

--- webarena/src/Model/Table/Fighterstable.php
+++ webarena/src/Model/Table/Fighterstable.php
@@ -113,20 +113,7 @@ class FightersTable extends Table {
     
     // Do we have the verify that an entity of fighters clearly refers to an entity of player and an entity of guilds ? (no foreign key violations)
 
-    /**
-     * 
-     * @param type $playerid
-     * @return boolean player entity or null if wrong parameter
-     */
-    public function getFighter($playerid){
-        if(is_string($playerid)){
-            $entity = $this->find()->where(['player_id =' => $playerid]);
-            return $entity;
-        }
-        return null;
-    }
-    
-    public function getBestFighter() {
+    function getBestFighter() {
        $bestfighter = $this->find("all")->max("level");
 
         return $bestfighter;
