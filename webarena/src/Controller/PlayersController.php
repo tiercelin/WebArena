ece12faf917d86aa6bcd7c696f6599fd92353663
Tiercelin Julie <julie.tiercelin@edu.ece.fr>
2017-10-25 16:11:26 +0200

    recuperation de password ok

--- webarena/src/Controller/PlayersController.php
+++ webarena/src/Controller/PlayersController.php
@@ -1,103 +1,105 @@
 <?php
 
 namespace App\Controller;
+
 use App\Controller\AppController;
 use Cake\ORM\TableRegistry;
 
 class PlayersController extends AppController {
+
     //Constant to define the lenght of the reseted password
     const PASSLENGTH = 15;
-    
+
     // Initialization 
-    public function initialize()
-    {
+    public function initialize() {
         $this->loadComponent('Flash');
     }
-    
+
     /**
      * Add a new player in database -> sign up function !
      */
-    public function newPlayer ()
-    {
+    public function newPlayer() {
         // Declare a new player entity
         $playersTable = TableRegistry::get('Players');
         $newPlayer = $playersTable->newEntity();
-        
+
         // Verify that this new player is unique, thanks to its email
         // First, retrieve all players with THIS email
-         $players = $this->Players->find()->where(['email = ' => $this->request->getData('email')]);
-              
-         // If the count equals 0, so there is no player in the database with this email
-         if ($players->count() == 0)
-         {  
-             if ($this->request->is('post'))
-             {
-                 // Merge form data with the new (enmpty) entity
-                 //$newPlayer = $this->Players->patchEntity($newPlayer, $this->request->getData());
-                 $newPlayer->email = $this->request->getData('email');
-                 $newPlayer->password = $this->request->getData('password');
-            
-                 // Then we save the new player in the database
-                 // If it works, redirect the user to the login page. If not, display an error message.
-                 if ($playersTable->save($newPlayer))
-                 {                    
-                     return $this->redirect (['controller' => 'players', 'action' => 'loginPlayer']);
-                 }
-                 $this->Flash->error(__('Account creation failure !'));
-             }
-         }   
-         else
-         {
-             $this->Flash->error(__('This account already exists !'));
-         }
+        $players = $this->Players->find()->where(['email = ' => $this->request->getData('email')]);
+
+        // If the count equals 0, so there is no player in the database with this email
+        if ($players->count() == 0) {
+            if ($this->request->is('post')) {
+                // Merge form data with the new (enmpty) entity
+                //$newPlayer = $this->Players->patchEntity($newPlayer, $this->request->getData());
+                $newPlayer->email = $this->request->getData('email');
+                $newPlayer->password = $this->request->getData('password');
+
+                // Then we save the new player in the database
+                // If it works, redirect the user to the login page. If not, display an error message.
+                if ($playersTable->save($newPlayer)) {
+                    return $this->redirect(['controller' => 'players', 'action' => 'loginPlayer']);
+                }
+                $this->Flash->error(__('Account creation failure !'));
+            }
+        } else {
+            $this->Flash->error(__('This account already exists !'));
+        }
     }
-    
+
     /**
      * This function verify email + password of the user in order to log in him if the data are correct
      * If so, set up session variable. If not, display an error message.
      */
-    public function loginPlayer ()
-    {
-        //$reset=false;
-        //$password="";
-        /*$password = $this->resetPassword();
-        $reset = $this->request->getData('resetpsw');
-        if($this->request->getData('resetpsw')){///does not display -> not going in if ?
-            $password = $this->resetPassword();
-        }*/
-        //$this->set('reset', $reset);
-        //$this->set('newPassword', $password);
+    public function loginPlayer() {
+        //$password = $this->resetPassword();
+        //$this->set('email', $email);
+        //$this->Players->save($myresetPwd);
+
+        $password = '';
+        $display = false;
+        $email2='';
         
-        if($this->request->is('post'))
-        {
+        if (!is_null($this->request->getData('reset'))) {
+            $email2='admin@test.com';
+            $email2 = $this->request->getData('resetemail');
+            $this->loadModel('Players');
+            $myresetPwd = $this->Players->getPlayer($email2);
+            
+            $password = $myresetPwd->password;
+            $display = true;
+            
+        }
+        $this->set('email2', $email2);
+        $this->set('password', $password);
+        $this->set('display', $display);
+
+        if ($this->request->is('post')) {
             // Retrieve all the players with the combinaison email + password entered (theoretically, only one result)
             $players = $this->Players->find()->where(['email = ' => $this->request->getData('email'), 'password = ' => $this->request->getData('password')]);
-            
+
             // If the player has been found in the database
-            if ($players->count() == 1)
-            {
+            if ($players->count() == 1) {
                 // Secondary check
                 $currentPlayer = $players->first();
-                
+
                 // Set session variable with the ID of the current player
                 $session = $this->request->session();
                 $session->write('playerId', $currentPlayer->id);
-                
+
                 // Redirect the user to the index page
-                return $this->redirect (['controller' => 'arenas', 'action' => 'index']);      
-            }
-            
-            else
-            {
+                return $this->redirect(['controller' => 'arenas', 'action' => 'index']);
+            } else {
                 $this->Flash->error(__('Authentification failed, please try again'));
             }
         }
     }
+
     //Code from stackoverflow to generate a random password, with a variable lentgh(here we use a constant)
-    /*public function resetPassword(){
+    public function resetPassword() {
         $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-=+;:,.?";
-       $password = substr( str_shuffle( $chars ), 0, self::PASSLENGTH );
-       return $password;
-    } */
+        $password = substr(str_shuffle($chars), 0, self::PASSLENGTH);
+        return $password;
+    }
 
 }
--- webarena/src/Template/Players/login_player.ctp
+++ webarena/src/Template/Players/login_player.ctp
@@ -4,15 +4,13 @@
 
 <?php
 echo $this->Form->create();
-echo $this->Form->input('email',array('type' => 'email','label' => 'Your email :'));
-echo $this->Form->input('password', array('type' => 'password', 'label' => 'Your password :'));
-echo $this->Form->submit('Sign in');
-echo $this->Form->end();
+echo $this->Form->input('email',array('type' => 'email','label' => 'Your email :', 'value'=>$email2));
+echo $this->Form->input('password', array('type' => 'password', 'label' => 'Your password :', 'value'=>$password));
+echo $this->Form->submit('Sign in', array('type' => 'submit', 'controller'=> 'Players', 'action'=>'loginPlayer', 'name'=>'Sign in'));
+echo $this->Form->end();?>
+<?= $this->Form->create(); echo $this->Form->input('resetemail',array('type' => 'email','label' => 'Your email :', 'value'=>$email2));
+echo $this->Form->postButton('Reset', ['controller' => 'Players','action' => 'loginPlayer'],['data' => ['reset' => 'true']]); 
+        $this->Form->end();if($display==true){
+            echo "your password is ".$password;
+        }
 ?>
-
-<?php /*echo $this->Form->postButton('Reset Password', ['controller' => 'Players','action' => 'loginPlayer', 'data'=>[ 'resetpsw'=>'true']]) */?>
-<?php/* echo $newPassword;
-if($reset==true){ //do not display
-    echo "your new password is ".$newPassword;
-    echo "<br>Think to note it somewhere to be able to acces your account !";
-*/}?>
