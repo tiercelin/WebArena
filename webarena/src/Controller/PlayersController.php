4adea60329cacff048544879bdf897a5ce56dc70
Tiercelin Julie <julie.tiercelin@edu.ece.fr>
2017-10-29 23:08:01 +0100

    bloqued simultaneous connexion by the same email

--- webarena/src/Controller/PlayersController.php
+++ webarena/src/Controller/PlayersController.php
@@ -65,10 +65,11 @@ class PlayersController extends AppController {
      * If so, set up session variable. If not, display an error message.
      */
     public function loginPlayer() {
-
+//change owd
         if ($this->request->is('post')) {
             $this->changePassword($this->request->getData('emailchange'), $this->request->getData('oldpassword'), $this->request->getData('newpassword'), $this->request->getData('checkpassword'));
         }
+        //reset pwd
         if ($this->request->is('post')) {
             $resetemail = $this->request->getData('emailreset');
             $player = $this->Players->find()->where(['email = ' => $resetemail])->first();
@@ -81,7 +82,7 @@ class PlayersController extends AppController {
             } else
                 $this->Flash->error(__('Wrong email, please try again'));
         }
-
+//sign in
         if ($this->request->is('post')) {
 
             // Retrieve all the players with the combinaison email + password entered (theoretically, only one result)
@@ -94,14 +95,15 @@ class PlayersController extends AppController {
                 if (password_verify($this->request->getData('password'), $players->first()->password)) {
                     // Secondary check
                     $currentPlayer = $players->first();
-
-                    // Set session variable with the ID of the current player
-                    $session = $this->request->session();
-                    $session->write('playerId', $currentPlayer->id);
-                    $this->addConnexionMultiplayer($this->request->session()->read('playerId'));
-                    $this->addConnection($this->request->session()->read('playerId'),$currentPlayer->email );
-                    // Redirect the user to the index page
-                    return $this->redirect(['controller' => 'arenas', 'action' => 'index']);
+                    if (!$this->isAlreadyConnected($currentPlayer)) {
+                        // Set session variable with the ID of the current player
+                        $session = $this->request->session();
+                        $session->write('playerId', $currentPlayer->id);
+                        $this->addConnexionMultiplayer($this->request->session()->read('playerId'));
+                        $this->addConnection($this->request->session()->read('playerId'), $currentPlayer->email);
+                        // Redirect the user to the index page
+                        return $this->redirect(['controller' => 'arenas', 'action' => 'index']);
+                    } $this->Flash->error(__('This user is already connected, try with another account!'));
                 } else {
                     $this->Flash->error(__('Wrong password, please try again'));
                 }
@@ -134,6 +136,7 @@ class PlayersController extends AppController {
         } else
             $this->Flash->error(__('Wrong email, please try again'));
     }
+
     /**
      * Add an event to the Events table when someone is connected
      * The Events will be used to know which user is connected to display it on the sight page !
@@ -147,7 +150,8 @@ class PlayersController extends AppController {
             'coordinate_y' => -2]);
         $this->Events->save($myNewEvent);
     }
-    public function addConnection($playerid, $playeremail){
+
+    public function addConnection($playerid, $playeremail) {
         $playersname = Text::tokenize($playeremail, '@');
         //get its fighter's id ton add the event
         $playersfighter = $this->Fighters->getFighter($playerid);
@@ -157,16 +161,34 @@ class PlayersController extends AppController {
                 'date' => Time::now(),
                 'coordinate_x' => $playersfighter->coordinate_x,
                 'coordinate_y' => $playersfighter->coordinate_y]);
-            
         } else {
             $myNewEvent = new Events([
                 'name' => $playersname[0] . ' connected',
                 'date' => Time::now(),
                 'coordinate_x' => 0,
                 'coordinate_y' => 0]);
-            
         }
         $this->Events->save($myNewEvent);
+    }
+
+    public function getUserConnected() {
+        $conn= array();
+        //get users connected
+        $newconnection = $this->Events->getConnexions();
+        if (!is_null($newconnection)) {
+            foreach ($newconnection as $nc) {
+                $conn[] = $nc->name; //the id of the player connected
+            }
+        }
+        return $conn;
+    }
+
+    public function isAlreadyConnected($currentplayer) {
+        $alreadyConnected = $this->getUserConnected();
+        if (in_array($currentplayer->id, $alreadyConnected)) {
+            return true;
+        } else
+            return false;
     }
 
 }
